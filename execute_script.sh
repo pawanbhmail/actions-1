5ead6b3b9b29d03f68a75af6572069fd83586851
pawanbhmail <pawan.bhmail@gmail.com>
2026-02-13 18:00:12 -0500

    fixing the errors in the workflow.

--- execute_script.sh
+++ execute_script.sh
@@ -2,6 +2,6 @@
 
 echo "Welcome to my work flow"
 sudo apt-get install cowsay -y
-cowsay -f dragon "I love gold, do you have any?" >> ascii_art.txy
+cowsay -f dragon "I love gold, do you have any?" >> ascii_art.txt
 cat ascii_art.txt
 ls -ltra
